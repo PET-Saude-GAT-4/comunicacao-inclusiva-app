66be37f610150e642c177e6d4559d9bab027d3c8
eduardor0cha <c.eduardo_rn@outlook.com>
2026-02-22 12:17:36 -0300

    fix: improve eslint and prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,16 @@
-import expoConfig from "eslint-config-expo/flat";
+import expoConfig from "eslint-config-expo/flat.js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
 
 export default defineConfig([
+  globalIgnores(["dist/*"]),
   expoConfig,
   eslintPluginPrettierRecommended,
   {
     ignores: ["dist/*"],
+    languageOptions: {
+      globals: globals.node,
+    },
   },
 ]);
