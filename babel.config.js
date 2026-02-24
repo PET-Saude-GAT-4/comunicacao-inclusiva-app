2904ebc26ef46270848fb097ddf21760b32d3ff3
NicoStone-dev <nicostone.rocha@gmail.com>
2026-02-24 11:02:13 -0300

    Merge Conflict fix

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,11 @@
-// Very much needed for a smaller bundle
-
 export default function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     env: {
       production: {
-        plugins: ['react-native-paper/babel'],
+        plugins: ["react-native-paper/babel"],
       },
     },
   };
-};
+}
