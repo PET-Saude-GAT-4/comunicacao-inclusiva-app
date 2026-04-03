4d9c4ae551fceb774e77ea5ec3a28690868b254c
ReizeXD <andersonreize@gmail.com>
2026-04-03 20:30:36 -0300

    feat: add types board and pictogram

---
+++ app/types/board.types.ts
@@ -0,0 +1,12 @@
+import { Pictogram } from "./pictogram.types";
+
+export interface BoardPictogram {
+  order: number;
+  pictogram: Pictogram; 
+}
+
+export interface Board {
+  id: number;
+  title: string;
+  items: BoardPictogram[];
+}
---
+++ app/types/pictogram.types.ts
@@ -0,0 +1,5 @@
+export interface Pictogram {
+  id: number;
+  description: string;
+  imageUrl: string;
+}
