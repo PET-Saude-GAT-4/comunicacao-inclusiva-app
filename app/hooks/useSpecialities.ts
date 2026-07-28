c59a5a07adcb7ae4261242b8612d8db952a0bfdb
ReizeXD <andersonreize@gmail.com>
2026-07-27 22:24:51 -0300

    fix: add profession id to find yours specialities

--- app/hooks/useSpecialities.ts
+++ app/hooks/useSpecialities.ts
@@ -3,26 +3,36 @@ import { Speciality } from "@/types/speciality.types";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
 
-const CACHE_KEY = "@specialities_cache";
-
-export function useSpecialities() {
+export function useSpecialities(professionId?: number | string) {
   const [specialities, setSpecialities] = useState<Speciality[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const loadSpecialities = async () => {
+    if (!professionId) {
+      setSpecialities([]);
+      setIsLoading(false);
+      return;
+    }
+    const cacheKey = `@specialities_cache_${professionId}`;
     try {
       // 1. Try to read from cache (where SyncEngine saves data)
-      const cacheData = await AsyncStorage.getItem(CACHE_KEY);
+      const cacheData = await AsyncStorage.getItem(cacheKey);
 
       if (cacheData) {
         setSpecialities(JSON.parse(cacheData));
       } else {
         // 2. If cache is completely empty, fall back to Mock
-        setSpecialities(specialityMock);
+        const filteredMock = specialityMock.filter(
+          (item) => item.professionId === Number(professionId),
+        );
+        setSpecialities(filteredMock);
       }
     } catch (error) {
       console.log("Failed to load specialities from cache. Using Mock.");
-      setSpecialities(specialityMock);
+      const filteredMock = specialityMock.filter(
+        (item) => item.professionId === Number(professionId),
+      );
+      setSpecialities(filteredMock);
     } finally {
       setIsLoading(false);
     }
@@ -30,7 +40,7 @@ export function useSpecialities() {
 
   useEffect(() => {
     loadSpecialities();
-  }, []);
+  }, [professionId]);
 
   return { specialities, isLoading, refetch: loadSpecialities };
 }
